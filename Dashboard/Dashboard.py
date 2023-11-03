0ac20d72607470bf567ea77337cb604673f08d0d
Muhammad Hamzah Fyrzatullah <70090797+muhamzahf@users.noreply.github.com>
2023-11-03 14:02:50 +0700

    Add files via upload

---
+++ Dashboard/Dashboard.py
@@ -0,0 +1,245 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# Set style seaborn
+sns.set(style='dark')
+
+# Menyiapkan data day_df
+day_df = pd.read_csv("day.csv")
+day_df.head()
+
+# Menghapus kolom yang tidak diperlukan
+drop_col = ['windspeed']
+
+for i in day_df.columns:
+  if i in drop_col:
+    day_df.drop(labels=i, axis=1, inplace=True)
+
+# Mengubah nama judul kolom
+day_df.rename(columns={
+    'dteday': 'dateday',
+    'yr': 'year',
+    'mnth': 'month',
+    'weathersit': 'weather_cond',
+    'cnt': 'count'
+}, inplace=True)
+
+# Mengubah angka menjadi keterangan
+day_df['month'] = day_df['month'].map({
+    1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
+    7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
+})
+day_df['season'] = day_df['season'].map({
+    1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'
+})
+day_df['weekday'] = day_df['weekday'].map({
+    0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'
+})
+day_df['weather_cond'] = day_df['weather_cond'].map({
+    1: 'Clear/Partly Cloudy',
+    2: 'Misty/Cloudy',
+    3: 'Light Snow/Rain',
+    4: 'Severe Weather'
+})
+
+
+# Menyiapkan daily_rent_df
+def create_daily_rent_df(df):
+    daily_rent_df = df.groupby(by='dateday').agg({
+        'count': 'sum'
+    }).reset_index()
+    return daily_rent_df
+
+# Menyiapkan daily_casual_rent_df
+def create_daily_casual_rent_df(df):
+    daily_casual_rent_df = df.groupby(by='dateday').agg({
+        'casual': 'sum'
+    }).reset_index()
+    return daily_casual_rent_df
+
+# Menyiapkan daily_registered_rent_df
+def create_daily_registered_rent_df(df):
+    daily_registered_rent_df = df.groupby(by='dateday').agg({
+        'registered': 'sum'
+    }).reset_index()
+    return daily_registered_rent_df
+    
+# Menyiapkan season_rent_df
+def create_season_rent_df(df):
+    season_rent_df = df.groupby(by='season')[['registered', 'casual']].sum().reset_index()
+    return season_rent_df
+
+# Menyiapkan monthly_rent_df
+def create_monthly_rent_df(df):
+    monthly_rent_df = df.groupby(by='month').agg({
+        'count': 'sum'
+    })
+    ordered_months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ]
+    monthly_rent_df = monthly_rent_df.reindex(ordered_months, fill_value=0)
+    return monthly_rent_df
+
+# Menyiapkan weekday_rent_df
+def create_weekday_rent_df(df):
+    weekday_rent_df = df.groupby(by='weekday').agg({
+        'count': 'sum'
+    }).reset_index()
+    return weekday_rent_df
+
+# Menyiapkan workingday_rent_df
+def create_workingday_rent_df(df):
+    workingday_rent_df = df.groupby(by='workingday').agg({
+        'count': 'sum'
+    }).reset_index()
+    return workingday_rent_df
+
+# Menyiapkan holiday_rent_df
+def create_holiday_rent_df(df):
+    holiday_rent_df = df.groupby(by='holiday').agg({
+        'count': 'sum'
+    }).reset_index()
+    return holiday_rent_df
+
+# Menyiapkan weather_rent_df
+def create_weather_rent_df(df):
+    weather_rent_df = df.groupby(by='weather_cond').agg({
+        'count': 'sum'
+    })
+    return weather_rent_df
+
+
+# Membuat komponen filter
+min_date = pd.to_datetime(day_df['dateday']).dt.date.min()
+max_date = pd.to_datetime(day_df['dateday']).dt.date.max()
+ 
+with st.sidebar:
+    st.image('https://ychef.files.bbci.co.uk/976x549/p014kc76.jpg')
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value= min_date,
+        max_value= max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = day_df[(day_df['dateday'] >= str(start_date)) & 
+                (day_df['dateday'] <= str(end_date))]
+
+# Menyiapkan berbagai dataframe
+daily_rent_df = create_daily_rent_df(main_df)
+daily_casual_rent_df = create_daily_casual_rent_df(main_df)
+daily_registered_rent_df = create_daily_registered_rent_df(main_df)
+season_rent_df = create_season_rent_df(main_df)
+monthly_rent_df = create_monthly_rent_df(main_df)
+weekday_rent_df = create_weekday_rent_df(main_df)
+workingday_rent_df = create_workingday_rent_df(main_df)
+holiday_rent_df = create_holiday_rent_df(main_df)
+weather_rent_df = create_weather_rent_df(main_df)
+
+
+# Membuat Dashboard secara lengkap
+
+# Membuat judul
+st.header('Bike Rental Dashboard')
+
+# Membuat jumlah penyewaan harian
+st.subheader('Daily Rentals')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    daily_rent_casual = daily_casual_rent_df['casual'].sum()
+    st.metric('Casual User', value= daily_rent_casual)
+
+with col2:
+    daily_rent_registered = daily_registered_rent_df['registered'].sum()
+    st.metric('Registered User', value= daily_rent_registered)
+ 
+with col3:
+    daily_rent_total = daily_rent_df['count'].sum()
+    st.metric('Total User', value= daily_rent_total)
+
+# Membuat jumlah penyewaan bulanan
+st.subheader('Monthly Rentals')
+fig, ax = plt.subplots(figsize=(24, 8))
+ax.plot(
+    monthly_rent_df.index,
+    monthly_rent_df['count'],
+    marker='o', 
+    linewidth=2,
+    color='tab:green'
+)
+
+for index, row in enumerate(monthly_rent_df['count']):
+    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
+ax.tick_params(axis='x', labelsize=25, rotation=45)
+ax.tick_params(axis='y', labelsize=20)
+st.pyplot(fig)
+
+# Membuat jumlah penyewaan berdasarkan season
+st.subheader('Seasonly Rentals')
+
+fig, ax = plt.subplots(figsize=(16, 8))
+
+sns.barplot(
+    x='season',
+    y='registered',
+    data=season_rent_df,
+    label='Registered',
+    color='tab:purple',
+    ax=ax
+)
+
+sns.barplot(
+    x='season',
+    y='casual',
+    data=season_rent_df,
+    label='Casual',
+    color='tab:blue',
+    ax=ax
+)
+
+for index, row in season_rent_df.iterrows():
+    ax.text(index, row['registered'], str(row['registered']), ha='center', va='bottom', fontsize=12)
+    ax.text(index, row['casual'], str(row['casual']), ha='center', va='bottom', fontsize=12)
+
+ax.set_xlabel(None)
+ax.set_ylabel(None)
+ax.tick_params(axis='x', labelsize=20, rotation=0)
+ax.tick_params(axis='y', labelsize=15)
+ax.legend()
+st.pyplot(fig)
+
+# Membuah jumlah penyewaan berdasarkan kondisi cuaca
+st.subheader('Weatherly Rentals')
+
+fig, ax = plt.subplots(figsize=(16, 8))
+
+colors=["tab:brown", "tab:green", "tab:orange"]
+
+sns.barplot(
+    x=weather_rent_df.index,
+    y=weather_rent_df['count'],
+    palette=colors,
+    ax=ax
+)
+
+for index, row in enumerate(weather_rent_df['count']):
+    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
+ax.set_xlabel(None)
+ax.set_ylabel(None)
+ax.tick_params(axis='x', labelsize=20)
+ax.tick_params(axis='y', labelsize=15)
+st.pyplot(fig)
+
+
+plt.tight_layout()
+st.pyplot(fig)
+
+st.caption('Copyright (c) Muhammad Hamzah Fyrzatullah 2023')
